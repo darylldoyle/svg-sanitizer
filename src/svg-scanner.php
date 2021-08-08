73ef18094b5d4ccbaa761a032f4c6f1febac88cc
Oliver Hader <oliver@typo3.org>
2021-08-08 15:50:16 +0200

    [BUGFIX] Ensure required sources are loaded in standalone scanner
    Fixes: #43

--- src/svg-scanner.php
+++ src/svg-scanner.php
@@ -12,8 +12,13 @@ require_once( __DIR__ . '/data/AttributeInterface.php' );
 require_once( __DIR__ . '/data/TagInterface.php' );
 require_once( __DIR__ . '/data/AllowedAttributes.php' );
 require_once( __DIR__ . '/data/AllowedTags.php' );
+require_once( __DIR__ . '/data/XPath.php' );
+require_once( __DIR__ . '/ElementReference/Resolver.php' );
+require_once( __DIR__ . '/ElementReference/Subject.php' );
+require_once( __DIR__ . '/ElementReference/Usage.php' );
+require_once( __DIR__ . '/Exceptions/NestingException.php' );
+require_once( __DIR__ . '/Helper.php' );
 require_once( __DIR__ . '/Sanitizer.php' );
-
 
 /*
  * Print array as JSON and then
